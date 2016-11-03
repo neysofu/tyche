0601e1619d6f5de2157458a7afd8e73a6489a6dd
Filippo Costa <filippocosta.italy@gmail.com>
2016-11-03 13:43:17 +0100

    Pages auto deploy, first try

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,27 @@ set -o errexit -o nounset
 
 if [ "$TRAVIS_BRANCH" != "master" ]
 then
-	echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+	echo "This commit was made against the $TRAVIS_BRANCH and not the master!"
 	exit 0
 fi
 
 # Short hash of HEAD
 rev=$(git rev-parse --short HEAD)
 
-cd target/latest/api
+cd target
+cd scala-2.11
+cd api
 
 git init
 git config user.name "Filippo Costa"
 git config user.email "filippocosta.italy@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/rust-lang/rust-by-example.git"
+git remote add upstream "https://$GH_TOKEN@github.com/neysofu/tyche.git"
 git fetch upstream
 git reset upstream/gh-pages
 
 touch .
 
 git add -A .
-git commit -m "rebuild pages at ${rev}"
+git commit -m "Rebuild pages at ${rev}"
 git push -q upstream HEAD:gh-pages
