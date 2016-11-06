79e1ec5b1d160f3c4cfe11aaa8f9179a250d94b0
Filippo Costa <filippocosta.italy@gmail.com>
2016-11-06 02:46:24 +0100

    Directory structure change

--- tyche-src/test/scala/io/neysofu/tyche/DiscreteUniformSpec.scala
+++ tyche-src/test/scala/io/neysofu/tyche/DiscreteUniformSpec.scala
@@ -6,17 +6,7 @@ import org.scalatest.{WordSpec, Matchers}
 class DiscreteUniformSpec extends WordSpec with Matchers {
  
   val sampleSize = 10000
-
-  // Coin toss
-  sealed trait Coin
-  object Head extends Coin
-  object Tail extends Coin
-  val coin = DiscreteDistribution.uniform(Head, Tail)
-
-  // 1-sided die
-  object Outcome
-  val die = DiscreteDistribution.uniform(Outcome)
-
+  
   // Families
   sealed trait Child
   object Male extends Child
@@ -27,25 +17,14 @@ class DiscreteUniformSpec extends WordSpec with Matchers {
 
   "A discrete uniform probability distribution" when {
     
-    "a coin is tossed" should {
-      "return $HEAD or $TAIL." in {
-        coin.get shouldBe a [Coin]
-      }
-      "take ~6 tosses to generate HH" in {
+      /*"take ~6 tosses to generate HH" in {
         val coinSpree = coin
           .until(_ containsSlice Seq(Head, Head))
           .map(_.size)
           .take(sampleSize)
           .sum
         Math.round(1.0 * coinSpree / sampleSize) shouldBe 6
-      }
-    }
-
-    "a 1-sided die is rolled" should {
-      "always return the same value" in {
-        die.get shouldBe Outcome
-      }
-    }
+      }*/
 
     "parents only want boys" should {
       "compute an expectation of one girl per family" in {
