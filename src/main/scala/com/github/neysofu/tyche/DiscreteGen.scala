36b430e621eb4dca43b9e7d5a72b08496c269627
Filippo Costa <filippocosta.italy@gmail.com>
2016-11-15 22:54:13 +0100

    Better scaladoc for Gen

--- src/main/scala/com/github/neysofu/tyche/DiscreteGen.scala
+++ src/main/scala/com/github/neysofu/tyche/DiscreteGen.scala
@@ -2,23 +2,22 @@ package com.github.neysofu.tyche
 
 /** A simple implementation trait for discrete random variables.
  */
-trait DiscreteGen[A] extends Gen[A] with MassFunction[A] with Moments[A] { self =>
+trait DiscreteGen[A] extends Gen[A] with MassFunction[A] with Moments[A] {
+    self =>
 
   def get: A = {
     val d = scala.util.Random.nextDouble
-    outcomes(cdf.indexWhere(_ > d))
+    outcomes(cdf indexWhere (_ > d))
   }
 
-  def virtualPlot(implicit toDouble: A <:< Double): Map[A, Double] = {
-    outcomes.zip(cdf).toMap
-  }
+  def virtualPlot(implicit toDouble: A <:< Double): Map[A, Double] =
+    (outcomes zip cdf).toMap
 
-  def mean(implicit toDouble: A <:< Double): Double = {
-    mass.map { case (k, v) => toDouble(k) * v }.sum
-  }
+  def mean(implicit toDouble: A <:< Double): Double =
+    (mass map {case (k, v) => toDouble(k) * v}).sum
 
   def standardDeviation(implicit toDouble: A <:< Double): Double = {
-    val m = mean
-    mass.map { case (k, v) => util.square(toDouble(k) - mean) * v }.sum
+    val cacheMean = mean
+    (mass map {case (k, v) => util.square(toDouble(k) - cacheMean) * v}).sum
   }
 } 
--- src/main/scala/com/github/neysofu/tyche/Gen.scala
+++ src/main/scala/com/github/neysofu/tyche/Gen.scala
@@ -3,119 +3,100 @@ package com.github.neysofu.tyche
 import scala.util.Random
 
 /** A Tyche generator is a high-level description of random variables and
- *  stochastic processes.
+ *  stochastic processes. Each and every instance is implemented by specifying
+ *  its generative function ([[com.github.neysofu.tyche.Gen.get]]).
  *
- *  Each and every instance is implemented by specifying its generative
- *  function ([[com.github.neysofu.tyche.Gen.get]]).
+ *  @define hanging
+ *  @note impossible predicates will result in the hanging of the program.
  *
- *  @example {{{
- *  scala> val uniform = new Gen[Double] {
- *       |   def get = scala.util.Random.nextDouble
- *       | }
- *  uniform: com.github.neysofu.tyche.Gen[Double] = \$...
- *
- *  }}}
- *
- *  @tparam A the outcomes' type signature. Note that many implementations
- *  require `A <: Double` to work correctly.
- *  
+ *  @tparam A the element type of this generator. Note that analysis methods
+ *  require `A <: Double` in order to work.
  *  @see [[com.github.neysofu.tyche.Moments]]
  */
-trait Gen[+A] { self =>
+trait Gen[+A] {
+    self =>
 
-  /** Returns a random outcome.
+  override def toString = "<generator>"
+
+  /** Computes a value. This method is known as the ''generative function''
+   *  of this generator.
    *
-   *  @example {{{
-   *  scala> uniform.get
-   *  res1: Double = 0.9749134509667129
-   *
-   *  }}}
+   *  @return a value.
    */
   def get: A
 
-  /** Builds a new generator by applying a functcom.github. to all the outcomes.
+  /** Builds a new generator by applying a function to each value.
    *  
-   *  @example {{{
-   *  scala> (uniform map (_+1)).get
-   *  res1: Double = 1.9153124129473624
-   *
-   *  }}}
+   *  @tparam B the range type of the given function `f`.
+   *  @param f the function to apply to each value.
+   *  @return a new generator resulting from replacing each value with the
+   *  output of the given function `f` with it as an input.
    */
   def map[B](f: A => B): Gen[B] = new Gen[B] {
     def get = f(self.get)
   }
 
-  /** Builds a new generator by filtering the sample space accordingly to a
-   *  predicate.
+  /** Builds a new generator by filtering values with a predicate.
    *
-   *  @example {{{
-   *  scala> (uniform filter (_ > 0.8)).get
-   *  res1: Double = 0.8433116704232428
-   *  
-   *  }}}
+   *  @param pred the predicate that each value must satisfy.
+   *  @return a new generator resulting from picking only values that satify
+   *  the given predicate `pred`.
+   *  $hanging
    */
   def filter(pred: A => Boolean): Gen[A] = map { x =>
-    def pickAnother(g: A): A = if (pred(g)) g else pickAnother(get)
-    pickAnother(x)
+    def rec(g: A): A = if (pred(g)) g else rec(get)
+    rec(x)
   }
 
-  /** Builds a new generator by creating a sample space which contains
-   *  only collections of outcomes that satisfy a predicate.
+  /** Builds a new generator by creating sequences of values which satisfy a
+   *  predicate.
    *
-   *  @example {{{
-   *  scala> (uniform until (_.sum > 1)).get
-   *  res1: Seq[Double] = List(0.8456141535917049, 0.5993270795832424)
-   *  
-   *  }}}
+   *  @param pred the predicate that each list must satisfy.
+   *  @return a new generator of lists resulting from appending values to an
+   *  empty list until it satisfies the given predicate `pred`.
+   *  $hanging
    */
-  def until(pred: Seq[A] => Boolean): Gen[Seq[A]] = map { x =>
-    def grow(ls: Seq[A]): Seq[A] = if (pred(ls)) ls else grow(ls :+ get)
-    grow(Seq(x))
+  def until(pred: List[A] => Boolean): Gen[List[A]] = map { x =>
+    def rec(ls: List[A]): List[A] = if (pred(ls)) ls else rec(ls :+ get)
+    rec(List(x))
   }
 
-  /** Builds a new generator by joining multiple outcomes together.
+  /** Builds a new generator by filling sequences with values.
    *
-   *  @example {{{
-   *  scala> uniform.repeat(2).get
-   *  res1: Seq[Double] = List(0.39691936209812506, 0.8606695143524133)
-   *  
-   *  }}}
+   *  @param n the length of each list.
+   *  @return a new generator resulting from replacing each value with a
+   *  `n`-sized list of values.
    */
-  def repeat(n: Int): Gen[Seq[A]] = map(_ +: take(n-1))
+  def repeat(n: Int): Gen[Seq[A]] = map(take(n-1) :+ _)
 
-  /** Builds a new bivariate generator by zipping the sample space with
-   *  another generator's.
+  /** Builds a new bivariate generator by coupling this generator with
+   *  another.
    *
-   *  @example {{{
-   *  scala> uniform.joint(uniform map (-_)).get
-   *  res1: (Double, Double) = (0.6916518719652441,-0.45253449818494484)
-   *  
-   *  }}}
+   *  @tparam B the element type of the given generator `that`.
+   *  @param that the generator to zip with this generator.
+   *  @return a new generator resulting from zipping each value with one of
+   *  the given generator `that`.
    */
   def joint[B](that: Gen[B]): Gen[(A, B)] = map((_, that.get))
 
-  /** Builds a new generator by replacing all the outcomes with their
-   *  respective numerical representation.
+  /** Builds a new generator by replacing each value with its numerical
+   *  representation.
+   *
+   *  @return a new generator resulting from replacing each value with its
+   *  floating point number representation.
    */
   def toGenDouble(implicit d: A <:< Double): Gen[Double] = map(d(_))
 
-  /** Returns an array of the desired length filled with random outcomes.
+  /** Computes a sequence filled with values.
    *
-   *  @example {{{
-   *  scala> uniform.take(2)
-   *  res1: Seq[Double] = List(0.3562246063380463, 0.4851991867143829)
-   *  
-   *  }}}
+   *  @param n the length of the returned list.
+   *  @return a `n`-sized list filled with values.
    */
   def take(n: Int): Seq[A] = Seq.fill(n)(get)
   
-  /** Returns a stream of outcomes.
+  /** Computes a stream of values.
    *
-   *  @example {{{
-   *  scala> uniform.toStream
-   *  res1: Stream[Double] = Stream(0.7792234677947281, ?)
-   *  
-   *  }}}
+   *  @return a stream of values.
    */
   def toStream: Stream[A] = get #:: toStream
 }  
